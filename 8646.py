96c0c33d9994ffc5ae49cb2cbc00dc35384cbf6e
Morlotus <44338735+Morlotus@users.noreply.github.com>
2018-10-25 00:54:37 +0400

    Project Start

---
+++ 8646.py
@@ -0,0 +1,153 @@
+Rings = (
+    (),  # index 0
+    (  # Ring 1
+        {'Number': '8646/027', 'Address': 'г.Красноярск, ул.Высотная, д.27, пом.141'},
+        {'Number': '8646/038', 'Address': 'г.Красноярск, Октябрьский район, ул.Словцова, д.16, пом. 290'},
+        {'Number': '8646/073', 'Address': 'г.Красноярск, ул.Тотмина, д.1Г, пом.108'},
+        {'Number': '8646/057', 'Address': 'г.Красноярск, ул.Калинина, д.8, пом.249, 250'},
+        {'Number': '8646/074', 'Address': 'г.Красноярск, пр-кт Свободный, 28 А, пом.100'},
+    ),
+    (  # Ring 2
+        {'Number': '8646/036', 'Address': 'г.Красноярск, ул.Копылова, д.48, пом.319'},
+        {'Number': '8646/088', 'Address': 'г.Красноярск, ул.Копылова, 76, комнаты 7-15'},
+        {'Number': '8646/056', 'Address': 'г.Красноярск, ул.Ленина, д.126, пом.61'},
+        {'Number': '8646/055', 'Address': 'г.Красноярск, ул.Республики, д.66, пом.6'},
+        {'Number': '8646/081', 'Address': 'г.Красноярск, ул.Красная Площадь, д.3а, пом.52'},
+        {'Number': '8646/085', 'Address': 'г.Красноярск, ул.Ленина, д.121, пом.27, 28, 30'},
+        {'Number': '8646/029', 'Address': 'г.Красноярск, ул.К.Маркса, д.112А, пом.45'},
+        {'Number': '8646/086', 'Address': 'г.Красноярск, ул.Карла Маркса, зд.125, пом.2, 4'},
+        {'Number': '8646/039', 'Address': 'г.Красноярск, Центральный район, ул.Сурикова, 12, строение 6, помещение 12'},
+        {'Number': '8646/060', 'Address': 'г.Красноярск, просп. Свободный, 46'},
+        {'Number': '8646/065', 'Address': 'г.Красноярск, пр-т Мира, д.56, комната № 1, 2, 21'},
+    ),
+    (  # Ring 3
+        {'Number': '8646/047', 'Address': 'г.Красноярск, ул.Краснодарская, д.8, пом.182'},
+        {'Number': '8646/072', 'Address': 'г.Красноярск, пр-т Металлургов, д.53'},
+        {'Number': '8646/071', 'Address': 'Не найден ВСП'},
+        {'Number': '8646/050', 'Address': 'г.Красноярск, ул.Тельмана, д.28 "В, пом.131, ком.54, 55'},
+        {'Number': '8646/054', 'Address': 'г.Красноярск, пр-т Металлургов, д.45А, пом.49'},
+        {'Number': '8646/048', 'Address': 'г.Красноярск, ул.Воронова, д.18, пом.294, ком.1-10'},
+        {'Number': '8646/051', 'Address': 'г.Красноярск, пр-т Молодежный, д. 7, пом. 364, лит. А,А1'},
+        {'Number': '8646/064', 'Address': 'г.Красноярск, ул.9 мая, д.24, пом.36, ком.12-31'},
+        {'Number': '8646/053', 'Address': 'г.Красноярск, ул.Водопьянова, 11'},
+        {'Number': '8646/062', 'Address': 'г.Красноярск, Советский район, ул.Молокова, 60, помещение 119'},
+        {'Number': '8646/070', 'Address': 'г.Красноярск, ул.78 Добровольческой бригады, 15, пом.25'},
+        {'Number': '8646/049', 'Address': 'г.Красноярск, ул.Партизана Железняка, д.19 "Г", пом.7'},
+        {'Number': '8646/046', 'Address': 'г.Красноярск, ул.78 Добровольческой бригады, д.3, пом.434'},
+    ),
+    (  # Ring 4
+        {'Number': '8646/084', 'Address': 'г.Красноярск, ул.Александра Матросова, зд.2, пом.9, 14, 28'},
+        {'Number': '8646/028', 'Address': 'г.Красноярск, ул. Александра Матросова, д.18, пом.57'},
+        {'Number': '8646/032', 'Address': 'г.Красноярск, ул.60 лет Октября, д.45, помещения № 95, 98'},
+        {'Number': '8646/035', 'Address': 'г.Красноярск, ул.Судостроительная, д.90, пом.465'},
+        {'Number': '8646/075', 'Address': 'г.Красноярск, ул.60 лет Октября, д.96 г, пом.195'},
+        {'Number': '8646/026', 'Address': 'г.Красноярск, просп.имени газеты Красноярский рабочий, д.181, пом.63'},
+        {'Number': '8646/040', 'Address': 'г.Красноярск, пр-кт им.газеты "Красноярский рабочий", д.83, пом.59'},
+    ),
+    (  # Ring 5
+        {'Number': '8646/044', 'Address': 'г.Красноярск, пр-кт им. газеты Красноярский рабочий, д.47, пом.135'},
+        {'Number': '8646/041', 'Address': 'г.Красноярск, пр-т им.газеты "Красноярский рабочий", д. 52, пом. 40'},
+        {'Number': '8646/042', 'Address': 'г.Красноярск, пр-т им.газеты "Красноярский рабочий", д.26, пом.62'},
+        {'Number': '8646/045', 'Address': 'г.Красноярск, ул.Шевченко, д.28, пом.96'},
+        {'Number': '8646/043', 'Address': 'г.Красноярск, ул.Львовская, д.35, пом.81'},
+        {'Number': '8646/025', 'Address': 'г.Красноярск, ул.Щорса, д.15, пом.55'},
+        {'Number': '8646/037', 'Address': 'г.Красноярск, ул.Пионерской Правды, д.8, пом.100'},
+    ),
+)
+
+
+def RingOutput(Rings):  # Вывод информации по кольцу
+    try:
+        print()
+        NumberRings = int(input('Please enter a ring number from 1 to 5: '))
+        if 0 < NumberRings <= 5:
+            print()
+            print('Information on', NumberRings, 'rings: ')
+            print('Total offices:', len(Rings[NumberRings]))
+            for i in Rings[NumberRings]:
+                print(i['Number'] + ', ' + i['Address'])
+                # print('Number: ' + i['Number'] + ' ' + 'Address: ' + i['Address'])
+        else:
+            print('ERROR! In Krasnoyarsk, only five rings, from 1 to 5')
+    except ValueError:
+        print('ERROR! Please indicate the ring number')
+
+
+# def WichRing(Rings):  # Вывод информации по одному ВСП
+#     while True:
+#         NumberVSP = str(input('\nTo exit - 0\nPlease enter the VSP number in the format "8646/044": '))
+#         x = 1
+#         while x < len(Rings):
+#             for i in Rings[x]:
+#                 if str(NumberVSP) == '0':
+#                     return
+#                 elif str(NumberVSP) == str(i['Number']):  # Нйденный
+#                     print('\n{0}th rings: {1}, {2}'.format(str(x), i['Number'], i['Address']))
+#                     break
+#             # else:
+#                 # print('{0}th rings: Office not found'.format(str(x)))
+#             x += 1
+
+
+def WichRing(Rings):  # Вывод информации по многим ВСП !разработка - ввод через проблем
+    officeInput = []
+    print("\nTo exit - 0\nFormat: '8646/04'\nPlease enter the VSP number one by one:\n ")
+    while True:
+        NumberVSP = str(input("To exit - 0, Format: '8646/04', number: "))
+        if NumberVSP != '0':
+            officeInput.append(NumberVSP)
+        else:
+            break
+    print(officeInput)
+    x = 1
+    while x < len(Rings):  # x - номер кольца
+        for i in Rings[x]:  # i - номер элемента в кольце
+            for y in officeInput:  # y - элемент в новом списке
+                # print('Кольцо', x)
+                # print('В кольце', Rings[x].index(i))
+                # print('В новом ', officeInput.index(y))
+                if str(y) == str((i['Number'])):
+                    # print(len(i['Number']))
+                    # print(type(len(i['Number'])))
+                    # print(str(i['Number']))
+                    print(str(i['Number'])[-2:len(i['Number'])])
+                    # print('\n{0}th rings: {1}, {2}'.format(str(x), i['Number'], i['Address']))
+                    print('\n%sth rings: %s, %s' % (x, i['Number'], i['Address']))
+        x += 1
+
+
+def AboutRings(Rings, x=1):  # Вывод информации по всем кольцам
+    while x <= 5:
+        # print('Rings:', x, 'Total:', str(len(Rings[x])).ljust(5, ' '), end="")
+        print('Rings %s Total: %s' % (x, str(len(Rings[x])).ljust(5, ' ')), end='')
+        for i in Rings[x]:
+            print('%s' % (i['Number'][-2:len(i['Number'])]), end=' ')
+            # print(i['Number'][-2:len(i['Number'])] + ' ', end='')
+        print()
+        x += 1
+
+
+print()
+print('Hello, network management center Samara\n')
+print('Opportunities:')
+print('1: Get information on the ring')  # RingOutput
+print('2: Get information: in which ring VSP')  # WichRing
+print('3: Get all information about Krasnoyarsk rings')  # AboutRings
+
+while True:
+    try:
+        print()
+        selection = int(input('4 - HELP. What do you want to do: '))
+        if selection == 1:
+            RingOutput(Rings)
+        elif selection == 2:
+            WichRing(Rings)
+        elif selection == 3:
+            AboutRings(Rings)
+        elif selection == 4:
+            print(
+                'HELP:\n1: Get information on the ring\n2: Get information: in which ring VSP\n3: Get all information about Krasnoyarsk rings')
+        else:
+            print('ERROR! Please enter numbers from 1 to 3')
+    except ValueError:
+        print('\n ERROR! Please enter numbers, not words')
---
+++ test.py
@@ -0,0 +1,58 @@
+# import re
+# i = str('а-б')
+#
+# if i == re.i():
+#     print('Ok')
+
+
+# a, b, c = map(int, input().split())
+
+# print(1, 2, 3, sep=', ')
+# a = 10
+# b = 5
+# print('У меня есть %s рублей %s копеек'%(a,b))
+
+
+a = 'Привет'
+#
+# print(a.ljust(10, '!'))
+# print(a.rjust(10, '!'))
+# print('Хай'.ljust(10, '%'))
+
+# w = 'Ivanov Ivan Ivanovich'
+# w.split()
+# a = w.split()
+# print(w)
+# print(a)
+# print(type(a))
+#
+# a, b, c = input().split()
+# print(a,b,c, end='')
+#
+# # i = '12346'
+# # print(i[-3:len(i) + 1])
+
+
+# a = input()
+# print(a)
+# print(a.replace('0', ''))
+# print(len(a.replace('0', '')))
+# print(
+#     type(len(a.replace('0', '')))
+# )
+#
+# inputVsp = input().replace('0', '').split()
+# print(inputVsp)
+# print(inputVsp[0])
+# print(inputVsp[0][-3:len(inputVsp[0])])
+
+
+inputVsp = input().replace('0', '').split()
+print(inputVsp)
+print(inputVsp[0])
+print(inputVsp[0][-3:len(inputVsp[0])])
+
+# inputVsp[0] = inputVsp[-3:len(inputVsp)]
+# print(inputVsp[0][-3:len(inputVsp)
+# searchVsp = inputVsp.split()[-3:len(inputVsp)]
+# print(searchVsp)
